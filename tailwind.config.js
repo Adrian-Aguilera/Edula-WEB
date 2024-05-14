2b6ab5fc0c7d6248a058a4b40c8771012167c51c
Fabio Castillo <fabiocastillo115@gmail.com>
2024-05-14 13:48:46 -0600

    agergando diseño para clase prueba de unity anexada

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.{html,css,js}"],
+  content: ["./public/*.{html,css,js}","./public/clases-unity/*.{html,css,js}"],
   theme: {
     extend: {},
   },
