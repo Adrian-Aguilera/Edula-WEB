7b8d95f6bd54f8df93318a18c1cb170d97553f29
Adrian-Aguilera <cruz74270@gmail.com>
2024-08-02 20:04:37 -0600

    fix: design button style

--- src/views/generalChat/generalChat.vue
+++ src/views/generalChat/generalChat.vue
@@ -1,18 +1,19 @@
 <template>
   <div>
     <ChatBox :messages="messages" :currentUser="currentUser" :isError="isError"/>
-
-    <form @submit.prevent="onSubmit" method="post"  class="flex items-center max-w-sm mx-auto">
-      <div class="relative w-full">
-        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"/>
-            </svg>
+    <div class="mt-6">
+      <form @submit.prevent="onSubmit" method="post"  class="flex items-center max-w-sm mx-auto">
+        <div class="relative w-full">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/>
+              </svg>
+          </div>
+          <input type="text" id="simple-search" v-model="formProps.message"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Pregunta algo..." required />
         </div>
-        <input type="text" id="simple-search" v-model="formProps.message"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Pregunta algo..." required />
+        <SubmitContent type="submit"></SubmitContent>
+      </form>
     </div>
-      <SubmitContent type="submit"></SubmitContent>
-    </form>
   </div>
 </template>
 
