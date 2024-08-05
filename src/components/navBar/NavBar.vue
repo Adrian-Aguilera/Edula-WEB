24f480c27bb27aa437c0fd0c9586a725190f2dee
Adrian-Aguilera <cruz74270@gmail.com>
2024-08-04 21:23:38 -0600

    fix: colors navbar

--- src/components/navBar/NavBar.vue
+++ src/components/navBar/NavBar.vue
@@ -1,12 +1,12 @@
 <template>
-    <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <nav class="bg-nav dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b  dark:border-gray-600">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-          <img :src="require('@assets/favicon.png')" class="h-8" alt="Itca Fepade">
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Itca Fepade</span>
+          <img :src="require('@assets/itca-blanco.png')" class="h-10" alt="Itca Fepade">
+          <!--<span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Itca Fepade</span>-->
         </a>
         <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <a href="https://apps.itca.edu.sv/" class="text-gray-900 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2">
+          <a href="https://apps.itca.edu.sv/" class="text-gray-900  bg-white hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-semibold rounded-xl text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2">
             ¿Estudiante?
             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -28,5 +28,8 @@ export default {
 <style>
     .flex-contents{
         display: contents !important;
+    }
+    .bg-nav{
+        background-color: brown;
     }
 </style>
