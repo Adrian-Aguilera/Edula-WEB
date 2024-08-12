e5bb034dc2bfd896f929f0b3b4868673c596d69c
Adrian-Aguilera <cruz74270@gmail.com>
2024-08-11 21:29:39 -0600

    fix: width input

--- src/views/generalChat/generalChat.vue
+++ src/views/generalChat/generalChat.vue
@@ -9,9 +9,9 @@
       <ChatBox :messages="messages" :currentUser="currentUser" :isError="isError" :isLoading="isLoading"/>
     </div>
     <div class="mt-2">
-      <form @submit.prevent="onSubmit" method="post" class="flex items-center max-w-sm mx-auto">
+      <form @submit.prevent="onSubmit" method="post" class="flex items-center w-2/5 mx-auto">
         <div class="relative w-full">
-          <input type="text" id="simple-search" v-model="formProps.message" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Pregunta algo..." required />
+          <input type="text" id="simple-search" v-model="formProps.message" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50  borderInput dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white " placeholder="Pregunta algo..." required />
           <SubmitContent type="submit"></SubmitContent>
         </div>
       </form>
@@ -77,3 +77,13 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.borderInput{
+  border-color: brown;
+}
+
+.borderInput:focus{
+  border-color: brown !important;
+}
+</style>
