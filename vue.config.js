19fa484d80517c0e4680805c22f9e7a6a69fbd88
Adrian-Aguilera <cruz74270@gmail.com>
2024-08-12 18:14:21 -0600

    ServerLocal: Configs

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,15 @@ const path = require('path');
 
 module.exports = {
   publicPath: '/',
-  configureWebpack: {
-    devServer: {
-      static: {
-        directory: path.join(__dirname, 'public'),
-      },
-      host: '0.0.0.0', // Permite que el servidor escuche en todas las interfaces de red
-      allowedHosts: 'all',
-      public: process.env.VUE_APP_PUBLIC_URL || 'localhost:8080',
-      port: process.env.VUE_APP_PORT || 8080,
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
     },
+    host: '0.0.0.0', // Permite que el servidor escuche en todas las interfaces de red
+    allowedHosts: 'all', // Permite cualquier host
+    port: process.env.VUE_APP_PORT || 8080,
+  },
+  configureWebpack: {
     resolve: {
       alias: {
         '@components': path.resolve(__dirname, 'src/components'),
