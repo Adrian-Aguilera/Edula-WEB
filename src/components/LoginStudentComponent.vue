8b34b3715ed5df59565d7c4db32a17078adee076
VICTUS-15\andre <andrewportillo5530@gmail.com>
2024-10-22 15:54:33 -0600

    se cambiaron los colores de los campos del formulario

--- src/components/LoginStudentComponent.vue
+++ src/components/LoginStudentComponent.vue
@@ -2,7 +2,7 @@
     <v-container fluid class="bg-surface-variant ConGeneral">
         <v-row no-gutters class="h-100">
             <v-col class="Con1" cols="12" md="4">
-                <v-sheet class="pa-0 ma-0  h-100 d-flex align-center justify-center">
+                <v-sheet class="pa-0 ma-0 h-100 d-flex align-center justify-center">
                     <v-card class="pa-6 pb-8 cardForm" elevation="1" max-width="400" rounded="lg">
                         <v-img
                             class="mx-auto my-6"
@@ -10,7 +10,10 @@
                             src="@/assets/logo-itca.avif"
                         ></v-img>
 
-                        <div class="text-subtitle-1 text-medium-emphasis">Carnet</div>
+                        <!-- Cambié text-red por text-red-500 -->
+                        <div class="text-subtitle-1 text-medium-emphasis ">
+                            <strong class="text-orange-darken-3">Carnet</strong> 
+                        </div>
 
                         <v-text-field
                             density="compact"
@@ -20,15 +23,14 @@
                         ></v-text-field>
 
                         <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-                            Contraseña
-                           
+                            <strong class="text-orange-darken-3">contraseña</strong> 
                         </div>
 
                         <v-text-field
                             :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
                             :type="visible ? 'text' : 'password'"
                             density="compact"
-                            placeholder="Enter your password"
+                            placeholder="Ingresa tu contraseña"
                             prepend-inner-icon="mdi-lock-outline"
                             variant="outlined"
                             @click:append-inner="visible = !visible"
@@ -42,7 +44,7 @@
 
                         <v-btn
                             class="mb-4"
-                            color="blue"
+                            color="red"
                             size="large"
                             variant="tonal"
                             block
@@ -52,7 +54,7 @@
 
                         <v-card-text class="text-center">
                             <a
-                                class="text-blue text-decoration-none"
+                                class="text-red text-decoration-none"
                                 href="#"
                                 rel="noopener noreferrer"
                                 target="_blank"
@@ -82,13 +84,13 @@ export default {
     padding: 0;
 }
 
-.caja2{
+.caja2 {
     background-image: url("@/assets/pc.jpg");
-    background-size: 100%;
+    background-size: cover; /* Cambiado a cover para cubrir completamente */
     background-position: center;
 }
 
-.cardForm{
+.cardForm {
     border: solid 1px #ff6a34;
 }
 </style>
