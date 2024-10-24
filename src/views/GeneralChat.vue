d77dcca4ee5ba1a71ee71ef1cf0b066f54c96a49
Alex-Il4 <oscarel.gtr@gmail.com>
2024-10-24 17:57:48 -0600

    Fix: Se hicieron cambios en el menu y cambios del footer en General Chat. Tambien se agregaron bucles en los cards para los mensajes

--- src/views/GeneralChat.vue
+++ src/views/GeneralChat.vue
@@ -1,91 +1,156 @@
 <template>
     <div class="background">
-      <div class="Menu">
-          <MenuComp/>
-      </div>
-      <div
-        id="goto-container-example"
-        class="overflow-auto custom-scrollbar"
-        style="max-height: 400px; margin-top: 25px"
-      >
-        <div class="mx-auto pa-12 pb-8 custom-card elevated"
-          style="max-width: 70%">
-          <v-card class="CardEduIA" style="margin-right: 45px;"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!"
-          title="EduIA"
-          flat
-          elevation="5"
-        ></v-card><br>
-          <v-card class="CardUser" style="margin-left: 45px;"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!"
-          title="Usuario"
-          flat
-        ></v-card><br>
-        <v-card class="CardEduIA" style="margin-right: 45px;"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!"
-          title="EduIA"
-          flat
-        ></v-card>
+        <div class="Menu">
+            <MenuComp />
         </div>
+        <div v-if="Usmessages.length > 0" id="goto-container-example" class="mx-auto overflow-auto custom-scrollbar" style="max-height: 400px; margin-top: 10px">
+      <div class="mx-auto pa-12 pb-8 custom-card" style="max-width: 100%">
+        <v-card class="CardEduIA" style="margin-right: 45px;" title="EduIA" flat elevation="5" v-for="(IAmessage, index) in messages" :key="index">
+          <p>{{ IAmessage }}</p>
+        </v-card>
+        <br>
+        <v-card class="CardUser" style="margin-left: 45px;" flat v-for="(Umessage, index) in Usmessages" :key="index">
+          <p>{{ Umessage }}</p>
+        </v-card>
+        <br>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import MenuComp from '@/components/MenuComp.vue';
-  export default {
+        <v-footer>
+            <v-row class="mx-auto custom-inputs">
+                <v-col>
+                    <v-card class="mx-auto cardInputs">
+                        <v-card-item>
+                            <v-row>
+                                <v-col>
+                                    <v-text-field solo hide-details flat class="chip-input"
+                                        :style="{ width: '100%', backgroundColor: '#bd9235' }" v-model="message"></v-text-field>
+                                </v-col>
+                                <v-col cols="auto">
+                                    <v-btn icon="$collapse" @click="sendMessage" variant="tonal"></v-btn>
+                                </v-col>
+                            </v-row>
+                        </v-card-item>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-footer>
+    </div>
+</template>
+
+<script>
+import MenuComp from '@/components/MenuComp.vue';
+export default {
     name: "GeneralChat",
     components: {
-      MenuComp,
+        MenuComp,
     },
-  }
-  </script>
-  
-  <style scoped>
-  .CardEduIA{
-    background-color: rgba(66, 66, 66, 0.253);
-    border: 1px solid #707070;
+    data() {
+    return {
+        message: '',
+        Usmessages: [],
+        UserMessage: ''
+    };
+  },
+  methods: {
+    sendMessage() {
+      this.UserMessage = this.message;
+      this.Usmessages.push(this.message);
+      this.message = '';
+    }
+  },
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Playwrite+GB+S:ital,wght@0,100..400;1,100..400&display=swap');
+
+.CardEduIA {
+    background-color: transparent;
+    border: 3px solid #e2a522;
     border-radius: 10px;
-    color: white;
-  }
-  .CardUser{
-    background-color: rgba(84, 104, 117, 0.253);
-    border: 1px solid #707070;
+    color: #e2a522;
+    box-shadow: 0 0 15px #63450f !important;
+    font-family: "Playwrite GB S", cursive;
+}
+
+.cardInputs {
+    background-color: rgba(187, 187, 187, 0.253);
+    border-radius: 20px;
+}
+
+.CardUser {
+    background-color: transparent;
+    border: 3px solid #bb1010;
     border-radius: 10px;
-    color: white;
-  }
-  .Menu{
+    color: #bb1010;
+    box-shadow: 0 0 15px #552b2b !important;
+    font-family: "Playwrite GB S", cursive;
+    margin-top: 15px !important;
+}
+
+.Menu {
     position: relative;
     background-color: transparent;
     width: auto;
     height: auto;
-    margin-top: 15px;
+    margin-top: 5px;
     border-radius: 10px;
-  }
-  .elevated {
-  box-shadow: 4px 0 6px 5px rgba(0, 0, 0, 0.4);
 }
-  .background{
+
+.background {
     position: relative;
-    background-color: rgb(32, 34, 44);
+    background-color: white;
     min-height: 100vh;
     padding: 16px;
     background-size: cover;
     background-position: center;
     z-index: 1;
-  }
-  .custom-card {
-    background-color: rgba(66, 66, 66, 0.253);
+}
+
+.custom-card {
+    background-color: rgba(187, 187, 187, 0.253);
     max-height: 50%;
-  }
-  .custom-scrollbar::-webkit-scrollbar {
+}
+
+.custom-scrollbar {
+    max-width: 70%;
+    border-radius: 20px;
+    box-shadow: 0 0 15px #666666;
+}
+
+.custom-scrollbar::-webkit-scrollbar {
     width: 7px;
-  }
-  .custom-scrollbar::-webkit-scrollbar-track {
-    background: #5e5e5e;
-  }
-  .custom-scrollbar::-webkit-scrollbar-thumb {
+}
+
+.custom-scrollbar::-webkit-scrollbar-track {
+    background: #5e5e5e !important;
+}
+
+.custom-scrollbar::-webkit-scrollbar-thumb {
     background: #bd9235;
-  }
-  </style>
-  
+}
+
+.custom-inputs {
+    max-width: 70%;
+    background-color: transparent;
+    margin-top: 5px;
+}
+
+.chip-input {
+    width: 100%;
+    border-radius: 20px;
+    color: rgb(255, 255, 255);
+    font-family: "Playwrite GB S", cursive;
+}
+
+.custom-btn {
+    background-color: #bd9235 !important;
+    color: aliceblue;
+    margin: 5px;
+    border: 1px solid #bd9235;
+}
+p{
+    color: black !important;
+    margin: 10px;
+}
+</style>
