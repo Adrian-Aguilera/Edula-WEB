325fdc9e04ff503b7a9b4089651341d80be6ad90
VICTUS-15\andre <andrewportillo5530@gmail.com>
2024-12-01 17:23:30 -0600

    se agrego el componente del chat a la nueva vista y se modifico la distribucion

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
     <div>
         <MenuComp />
+        <studentChatComponent />
     </div>
 </template>
 
 <script>
 
 import MenuComp from '@/components/MenuComp.vue';
+import StudentChatComponent from '@/components/StudentChatComponent.vue';
 
 export default {
   name: "homeView",
   components: {
     MenuComp,
+    StudentChatComponent,
  
   },
 };
