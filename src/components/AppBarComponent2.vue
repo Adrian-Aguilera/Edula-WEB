348df135bedd80e0d95caf943010d812d50d538b
Andrew\Andrew Llanos <andrewportillo5530@gmail.com>
2025-01-15 20:00:32 -0600

    se arreglo el flujo de iniciar sesion y de cerrar sesion de estudiante

---
+++ src/components/AppBarComponent2.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-app-bar
+        :order="order"
+        color="red-darken-2"
+        flat
+    >
+        <v-img
+            class="ml-8 bg-white"
+            rounded
+            max-width="50"
+            src="https://www.itca.edu.sv/wp-content/themes/elaniin-itca/images/favicon.png"
+            height="50"
+            contain
+        />
+        <template v-slot:append>
+            <v-chip
+                class="ma-2"
+                :color=" this.$store.getters.theme === 'light' ? 'orange' : 'black'"
+                @click="changeTheme"
+                variant="flat"
+            >
+                <v-icon :icon=" this.$store.getters.theme === 'light' ? 'bi bi-brightness-high-fill' : 'bi bi-moon-stars-fill'" :color=" this.$store.getters.theme === 'light' ? 'white' : 'orange'"></v-icon>
+                <span class="ml-2">{{ this.$store.getters.theme === 'light' ? 'Dark' : 'Light' }}</span>
+            </v-chip>
+        </template>
+        <v-toolbar-title>Bienvenido a Edula</v-toolbar-title>
+        <v-btn rounded="xl" size="x-large" @click="Login">Cerrar Sesion</v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: "AppBarComponent2",
+    data: () => ({
+    }),
+    methods: {
+        changeTheme() {
+            this.$store.dispatch('setTheme', this.$store.getters.theme === 'light' ? 'dark' : 'light')
+        },
+        Login(){
+            this.$router.push('/');
+        }
+    },
+}
+</script>
